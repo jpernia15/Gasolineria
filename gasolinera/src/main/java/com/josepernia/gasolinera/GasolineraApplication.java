1163d5b6fc8cf20b35bcb001378e0cbe710698f9
jpernia15 <yaonlivvonce287@gmail.com>
2025-01-25 16:00:17 -0400

    gasolinera de spring

---
+++ gasolinera/src/main/java/com/josepernia/gasolinera/GasolineraApplication.java
@@ -0,0 +1,13 @@
+package com.josepernia.gasolinera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasolineraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasolineraApplication.class, args);
+	}
+
+}
